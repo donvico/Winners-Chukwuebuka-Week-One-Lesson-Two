ecdd731ff7df2a0e81d030ad950fe8fd24d10170
donvico <wvictoryc@gmail.com>
2023-06-23 11:51:48 +0100

    commit week 2 files

---
+++ tasks/task_five.js
@@ -0,0 +1,7 @@
+//using tenary
+const digit = 4
+
+digit > 10
+
+  ? console.log('number is greater than 10.') //
+  : console.log('number is less than or equal to 10.') //using a lesser number than 10 will produce the output in the bracket before
---
+++ tasks/task_six.js
@@ -0,0 +1,6 @@
+const number = 12
+
+number > 10
+
+  ? console.log('number is greater than 10.') // @ "number is greater than 10."
+  : console.log('number is less than or equal to 10.')
